4f1be186f284b555ce1bb0b7b09b6f572d7ae81b
56333178 <56333178@qq.com>
2019-03-08 18:33:12 +0800

    增加swagger

--- src/main/java/com/yyh/authoritymanagement/controller/CommonController.java
+++ src/main/java/com/yyh/authoritymanagement/controller/CommonController.java
@@ -1,16 +1,19 @@
 package com.yyh.authoritymanagement.controller;
 
 import com.yyh.authoritymanagement.util.SMSUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Api(tags = "验证码接口")
 @RestController
 public class CommonController {
     @RequestMapping("/yzm")
-    public String sendYzm(@RequestParam(value = "mobilephone",required = true)String mobilephone, HttpServletRequest request){
+    public String sendYzm(@ApiParam(name = "mobilephone", value = "手机号码")@RequestParam(value = "mobilephone",required = true)String mobilephone, HttpServletRequest request){
         int yzm=(int)((Math.random()+1)*1000);
         request.getSession().setAttribute("yzm",yzm);
         System.out.println("验证码为："+yzm);
